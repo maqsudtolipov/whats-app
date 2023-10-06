bd8a61db6326bf77c332cde749728b8c46a50c45
Maqsud Tolipov <maqsudtolipov9@gmail.com>
2023-10-07 03:00:36 +0500

    📦 Setup prettier and eslint

--- server/app.js
+++ server/app.js
@@ -1,22 +1,23 @@
 const express = require('express');
-const dotEnv = require('dotenv')
+const dotEnv = require('dotenv');
 const mongoose = require('mongoose');
 
-dotEnv.config({path: './.env'})
+dotEnv.config({ path: './.env' });
 
 const app = express();
 
-
 const DB = process.env.DATABASE.replace('<PASSWORD>', process.env.DB_PASS);
-mongoose.connect(DB).then(() => console.log('☁️  Database connection successful!'))
+mongoose
+  .connect(DB)
+  .then(() => console.log('☁️  Database connection successful!'));
 
 app.get('', (req, res) => {
-	res.json({
-		success: 'success',
-		message: 'Hello there'
-	})
-})
+  res.json({
+    success: 'success',
+    message: 'Hello there',
+  });
+});
 
 app.listen(process.env.PORT || 8000, () => {
-	console.log(`⛵️ Listening on port ${process.env.PORT || 8000}`);
-})
+  console.log(`⛵️ Listening on port ${process.env.PORT || 8000}`);
+});
