eb78daec7be88d5148c5690c8c11ac4a46b72fd5
Maqsud Tolipov <maqsudtolipov9@gmail.com>
2023-10-08 03:37:38 +0500

    ☁️ Add sender to DM model

--- server/controllers/directMessageController.js
+++ server/controllers/directMessageController.js
@@ -2,10 +2,10 @@ const catchAsync = require('../utils/catchAsync');
 const DirectMessage = require('../models/directMessageModel');
 
 exports.sendDM = catchAsync(async (req, res) => {
-  console.log(req.params);
   const conversation = await DirectMessage.create({
     content: req.body.content,
     conversation: req.params.id,
+    sender: req.body.sender,
   });
 
   res.status(200).json({
--- server/models/directMessageModel.js
+++ server/models/directMessageModel.js
@@ -10,6 +10,11 @@ const directMessageSchema = new mongoose.Schema({
     ref: 'Conversation',
     required: [true, 'Please select a conversation'],
   },
+  sender: {
+    type: mongoose.Schema.ObjectId,
+    ref: 'User',
+    required: [true, 'Are you stupid??'],
+  },
   createdAt: {
     type: Date,
     default: Date.now(),
