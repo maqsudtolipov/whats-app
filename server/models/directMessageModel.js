33e094f900c6f9c92805b7be14520a9eff72b842
Maqsud Tolipov <maqsudtolipov9@gmail.com>
2023-10-08 02:22:25 +0500

    ☁️ Create DM model

---
+++ server/models/directMessageModel.js
@@ -0,0 +1,17 @@
+const mongoose = require('mongoose');
+
+const directMessageSchema = new mongoose.Schema({
+  content: {
+    type: String,
+    required: [true, 'Message cannot be empty'],
+  },
+  conversation: {
+    type: mongoose.Schema.ObjectId,
+    ref: 'Conversation',
+    required: [true, 'Please select a conversation'],
+  },
+  createdAt: Date.now(),
+});
+
+const DirectMessage = mongoose.model('DirectMessage', directMessageSchema);
+module.exports = DirectMessage;
