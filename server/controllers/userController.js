636c2603af8ba4b6b2d818d20668bf891c79f485
Maqsud Tolipov <maqsudtolipov9@gmail.com>
2023-10-08 02:08:33 +0500

    ☁️ Create catchAsync function

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -1,10 +1,11 @@
 const User = require('../models/userModel');
+const catchAsync = require('../utils/catchAsync');
 
-exports.getUsers = async (req, res, next) => {
+exports.getUsers = catchAsync(async (req, res, next) => {
   const users = await User.find();
 
   res.status(200).json({
     status: 'success',
     data: users,
   });
-};
+});
