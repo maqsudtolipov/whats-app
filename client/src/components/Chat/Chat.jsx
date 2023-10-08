ad32835e7781fa0854d37a7744f31a7c402473c9
Maqsud Tolipov <maqsudtolipov9@gmail.com>
2023-10-09 04:44:09 +0500

    🎨 Design chat profile

--- client/src/components/Chat/Chat.jsx
+++ client/src/components/Chat/Chat.jsx
@@ -1,5 +1,20 @@
+import './Chat.scss';
+import { RiMore2Line, RiSearch2Line } from 'react-icons/ri';
+
 const Chat = () => {
-  return <div>Chat</div>;
+  return (
+    <div className="chat">
+      <div className="chat__profile">
+        <img
+          src="https://xsgames.co/randomusers/assets/avatars/male/23.jpg"
+          alt="Chat group"
+        />
+        <div className="chat__profile-name">Luis Suarez</div>
+        <RiSearch2Line />
+        <RiMore2Line />
+      </div>
+    </div>
+  );
 };
 
 export default Chat;
