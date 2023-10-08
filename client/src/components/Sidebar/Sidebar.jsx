c233ca6ed201d2f2f946c93cf58d3f595f1aeb43
Maqsud Tolipov <maqsudtolipov9@gmail.com>
2023-10-08 22:03:33 +0500

    👨 Add profile part to sidebar

--- client/src/components/Sidebar/Sidebar.jsx
+++ client/src/components/Sidebar/Sidebar.jsx
@@ -1,5 +1,33 @@
+import './Sidebar.scss';
+import {
+  RiChatNewLine,
+  RiMore2Line,
+  RiSearch2Fill,
+  RiSearch2Line,
+} from 'react-icons/ri';
+
 const Sidebar = () => {
-  return <div>Sidebar</div>;
+  return (
+    <div className="sidebar">
+      <div className="sidebar__profile">
+        <img
+          src="https://randomuser.me/api/portraits/men/43.jpg"
+          alt="Profile"
+        />
+        <div className="sidebar__profile-icons">
+          <RiChatNewLine />
+          <RiMore2Line />
+        </div>
+      </div>
+
+      <div className="sidebar__search">
+        <form>
+          <input type="text" />
+          <RiSearch2Line />
+        </form>
+      </div>
+    </div>
+  );
 };
 
 export default Sidebar;
