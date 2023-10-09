8fcae419af253d9569414bbab3e2580af215c5c2
Maqsud Tolipov <maqsudtolipov9@gmail.com>
2023-10-09 18:53:47 +0500

    🍪 Generate and send token via Cookie

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -1,5 +1,28 @@
 const catchAsync = require('./../utils/catchAsync');
 const User = require('./../models/userModel');
+const jwt = require('jsonwebtoken');
+
+const signToken = (id) =>
+  jwt.sign({ id }, process.env.JWT_SECRET, {
+    expiresIn: '30d',
+  });
+
+const createSendToken = (user, statusCode, req, res) => {
+  const token = signToken(user.id);
+
+  const cookieOptions = {
+    expires: new Date(Date.now() + 30 * 24 * 60 * 60 * 1000),
+    withCredentials: true,
+  };
+  res.cookie('jwt', token, cookieOptions);
+
+  user.password = undefined;
+
+  res.status(statusCode).json({
+    status: 'success',
+    data: user,
+  });
+};
 
 exports.signUp = catchAsync(async (req, res, next) => {
   const userData = {
@@ -11,8 +34,5 @@ exports.signUp = catchAsync(async (req, res, next) => {
 
   const user = await User.create(userData);
 
-  res.status(200).json({
-    status: 'success',
-    data: user,
-  });
+  createSendToken(user, 201, req, res);
 });
