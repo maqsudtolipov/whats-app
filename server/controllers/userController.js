19dd987a76dca2ef408e704ccbba880e1534779d
Maqsud Tolipov <maqsudtolipov9@gmail.com>
2023-10-12 02:56:31 +0500

    🔎 Search users by name

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -2,7 +2,12 @@ const User = require('../models/userModel');
 const catchAsync = require('../utils/catchAsync');
 
 exports.getUsers = catchAsync(async (req, res, next) => {
-  const users = await User.find();
+  const { name } = req.query;
+  const query = {};
+
+  if (name) query.name = { $regex: name, $options: 'ix' };
+
+  const users = await User.find(query);
 
   res.status(200).json({
     status: 'success',
