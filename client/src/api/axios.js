1c09e310e928ce2dca87ec5dce45bbb7ac14a04b
Maqsud Tolipov <maqsudtolipov9@gmail.com>
2023-10-12 04:19:41 +0500

    🚀 Update axios.js

--- client/src/api/axios.js
+++ client/src/api/axios.js
@@ -1,6 +1,9 @@
 import axios from 'axios';
 
 export default axios.create({
-  baseURL: 'http://localhost:8000/api',
+  baseURL:
+    process.env.NODE_ENV === 'development'
+      ? 'http://localhost:8000/api'
+      : 'https://whats-app-ktfi.onrender.com/api',
   withCredentials: true,
 });
